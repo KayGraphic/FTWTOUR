03ca2336f0e96d14d75269c34e3163fa0f53a351
kay gregg <kaygraph@gmail.com>
2018-07-06 16:14:06 -0400

    Update MainActivity.java
    cleaned up spaces

--- app/src/main/java/com/example/android/ftwtour/MainActivity.java
+++ app/src/main/java/com/example/android/ftwtour/MainActivity.java
@@ -1,6 +1,4 @@
 package com.example.android.ftwtour;
-
-
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -11,18 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-
-        // CategoryFragmentPagerAdapter adapter =
-        //     new CategoryFragmentPagerAdapter(getSupportFragmentManager());
-
         viewPager.setAdapter(new CatagoryFragmentPagerAdapter(getSupportFragmentManager(),
                 MainActivity.this));
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
